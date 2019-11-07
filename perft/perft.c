cc9e8546b680fcbfb425a06cdd4ffa4a6c66933f
Valera Rozuvan <valera.rozuvan@gmail.com>
2019-11-07 17:41:57 +0200

    Tweaking perft. Adding docs, and results.

--- perft/perft.c
+++ perft/perft.c
@@ -63,20 +63,36 @@ int main()
   BOARD cBoard;
   unsigned long long count = 0ULL;
   unsigned char bailout = 0;
+  unsigned char idx = 0;
 
-  char *perftPos = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+  // char *perftPos = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
   // char *perftPos = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -";
   // char *perftPos = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -";
   // char *perftPos = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1";
   // char *perftPos = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
   // char *perftPos = "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10";
 
-  printf("%s\n", perftPos);
-  setPositionFromFen(&cBoard, perftPos);
+  char *perftPos[6] = {
+    "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+    "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -",
+    "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -",
+    "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
+    "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
+    "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10"
+  };
 
-  for (bailout = 0; bailout < 6; bailout += 1) {
+  // for (bailout = 0; bailout < 6; bailout += 1) {
+  //   count = countMoves(&cBoard, bailout, 0);
+  //   printf("depth %hhu: perft count = %llu\n", bailout + 1, count);
+  // }
+
+  bailout = 5;
+
+  for (idx = 0; idx < 6; idx += 1) {
+    printf("%s\n", perftPos[idx]);
+    setPositionFromFen(&cBoard, perftPos[idx]);
     count = countMoves(&cBoard, bailout, 0);
-    printf("depth %hhu: perft count = %llu\n", bailout + 1, count);
+    printf("depth %hhu: perft count = %llu\n\n", bailout + 1, count);
   }
 
   return 0;
